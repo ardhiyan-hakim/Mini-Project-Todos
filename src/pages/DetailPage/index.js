b24a9afdf12e628012c0221c21085e89df31c398
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-10-07 14:50:32 +0700

    Initial Setting Router

---
+++ src/pages/DetailPage/index.js
@@ -0,0 +1,5 @@
+function DetailPage() {
+  return <h1>Ini Home Page - PAGES</h1>;
+}
+
+export default DetailPage;
---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,5 @@
+function NotFoundPage() {
+  return <h1>Ini Not Found Page - PAGES</h1>;
+}
+
+export default NotFoundPage;
